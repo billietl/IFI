29bb2c4c7299a56025637ad55d8d4c3f2ec0cd24
Louis Billiet <louis.billiet59@gmail.com>
2013-09-23 11:06:25 +0200

    Ajout des interfaces DAO

---
+++ Music/src/fr/univlille1/tiir/music/dao/Share.java
@@ -0,0 +1,5 @@
+package fr.univlille1.tiir.music.dao;
+
+public interface Share {
+
+}
---
+++ Music/src/fr/univlille1/tiir/music/dao/User.java
@@ -0,0 +1,9 @@
+package fr.univlille1.tiir.music.dao;
+
+import java.io.Serializable;
+
+public interface User extends Serializable {
+	
+	public String toString();
+	
+}
