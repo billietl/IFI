a7f275e45b4240ddcea4ddca1be558170876bc80
Louis Billiet <louis.billiet59@gmail.com>
2013-09-23 11:42:05 +0200

    Précision de Music

--- Music/src/fr/univlille1/tiir/music/dao/Music.java
+++ Music/src/fr/univlille1/tiir/music/dao/Music.java
@@ -1,8 +1,14 @@
 package fr.univlille1.tiir.music.dao;
 
+import java.io.File;
 import java.io.Serializable;
 
 public interface Music extends Serializable {
 
+	public File getPath();
+	public String getTitre();
+	public void setTitre(String titre);
+	public String getArtiste();
+	public void setArtiste(String artiste);
 	public String toString();
 }
--- Music/src/fr/univlille1/tiir/music/dao/impl/MusicDAO.java
+++ Music/src/fr/univlille1/tiir/music/dao/impl/MusicDAO.java
@@ -1,32 +1,53 @@
 package fr.univlille1.tiir.music.dao.impl;
 
+import java.io.File;
+
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class MusicDAO implements fr.univlille1.tiir.music.dao.Music{
 
 	private static final long serialVersionUID = 1L;
-	
+
 	// Persistent Fields:
-    @Id
-    @GeneratedValue
-    Long id;
-    private String titre;
- 
-    // Constructors:
-    public MusicDAO() {
-    }
- 
-    public MusicDAO(String titre) {
-        this.titre = titre;
-    }
- 
-    // String Representation:
-    @Override
-    public String toString() {
-        return "Musique : " + titre;
-    }
-    
+	@Id
+	private File path;
+	private String titre;
+	private String artiste;
+
+	// Constructors:
+	public MusicDAO() {
+	}
+
+	public MusicDAO(String titre) {
+		this.titre = titre;
+	}
+
+	// String Representation:
+	@Override
+	public String toString() {
+		return "Musique : " + titre;
+	}
+
+	public File getPath() {
+		return this.path;
+	}
+
+	public String getTitre() {
+		return this.titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public String getArtiste() {
+		return this.artiste;
+	}
+
+	public void setArtiste(String artiste) {
+		this.artiste = artiste;
+	}
+
 }
