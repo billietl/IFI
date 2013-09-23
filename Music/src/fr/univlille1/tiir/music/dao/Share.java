8316c2b7e07fa58bb6b4145294e5d04d8f108219
Louis Billiet <louis.billiet59@gmail.com>
2013-09-23 10:30:03 +0200

    Ajout des entités.

---
+++ Music/src/fr/univlille1/tiir/music/dao/Share.java
@@ -0,0 +1,11 @@
+package fr.univlille1.tiir.music.dao;
+
+import java.io.Serializable;
+import java.sql.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class Share {
+
+}
---
+++ Music/src/fr/univlille1/tiir/music/dao/User.java
@@ -0,0 +1,32 @@
+package fr.univlille1.tiir.music.dao;
+
+import java.io.Serializable;
+import java.sql.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	// Persistent Fields:
+    @Id
+    @GeneratedValue
+    Long id;
+    private String pseudo;
+ 
+    // Constructors:
+    public User() {
+    }
+ 
+    public User(String pseudo) {
+        this.pseudo = pseudo;
+    }
+ 
+    // String Representation:
+    @Override
+    public String toString() {
+        return "Utilisateur : " + pseudo;
+    }
+}
