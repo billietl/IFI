d641c2432159df718ae9035ceb4dbddb2126baf4
Louis Billiet <louis.billiet59@gmail.com>
2013-09-23 11:47:54 +0200

    Ajout du getLibrary dans UserDAO

--- Music/src/fr/univlille1/tiir/music/dao/User.java
+++ Music/src/fr/univlille1/tiir/music/dao/User.java
@@ -1,11 +1,14 @@
 package fr.univlille1.tiir.music.dao;
 
 import java.io.Serializable;
+import java.util.List;
 
 public interface User extends Serializable {
 	
 	public String getNickName();
-	
+	public String getPasswd();
+	public void setPasswd(String passwd);
+	public List<Music> getLibrary();
 	public String toString();
 	
 }
--- Music/src/fr/univlille1/tiir/music/dao/impl/UserDAO.java
+++ Music/src/fr/univlille1/tiir/music/dao/impl/UserDAO.java
@@ -5,33 +5,47 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import fr.univlille1.tiir.music.dao.*;
+
 @Entity
 public class UserDAO implements fr.univlille1.tiir.music.dao.User{
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	// Persistent Fields:
-    @Id
-    private String pseudo;
-    private String passwd;
-    private List<fr.univlille1.tiir.music.dao.Music> bibliothèque; // relation avec Music (Own)
- 
-    // Constructors:
-    public UserDAO() {
-    }
- 
-    public UserDAO(String pseudo, String passwd) {
-        this.pseudo = pseudo;
-        this.passwd = passwd;
-    }
- 
-    // String Representation:
-    @Override
-    public String toString() {
-        return "Utilisateur : " + pseudo;
-    }
+	@Id
+	private String pseudo;
+	private String passwd;
+	private List<Music> bibliothèque; // relation avec Music (Own)
+
+	// Constructors:
+	public UserDAO() {
+	}
+
+	public UserDAO(String pseudo, String passwd) {
+		this.pseudo = pseudo;
+		this.passwd = passwd;
+	}
+
+	// String Representation:
+	@Override
+	public String toString() {
+		return "Utilisateur : " + pseudo;
+	}
 
 	public String getNickName() {
 		return this.pseudo;
+	}
+
+	public String getPasswd() {
+		return this.passwd;
+	}
+
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	
+	public List<Music> getLibrary(){
+		return this.bibliothèque;
 	}
 }
