22111745c4848fdc566205e60bad3cab97c868af
Louis Billiet <louis.billiet59@gmail.com>
2013-09-30 10:04:29 +0200

    precision de la classe User

--- Music/src/fr/univlille1/tiir/music/dao/impl/User.java
+++ Music/src/fr/univlille1/tiir/music/dao/impl/User.java
@@ -1,32 +1,35 @@
 package fr.univlille1.tiir.music.dao.impl;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class User implements fr.univlille1.tiir.music.dao.User {
 
+	@Id
+	private long id;
+	private String nom;
+	private String prenom;
+	private String pseudo;
+	
 	@Override
 	public long getId() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.id;
 	}
 
 	@Override
 	public String getNom() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.nom;
 	}
 
 	@Override
 	public String getPrenom() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.prenom;
 	}
 
 	@Override
 	public String getPseudo() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.pseudo;
 	}
 
 }
