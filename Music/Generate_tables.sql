e8cdde758917546febbdada3e87722498b3e8ee0
Louis Billiet <louis.billiet59@gmail.com>
2013-10-07 10:35:13 +0200

    génération des tables en base

---
+++ Music/Generate_tables.sql
@@ -0,0 +1,28 @@
+create table Musique(
+	musique_id bigint,
+	path varchar(50),
+	titre varchar(50),
+	artiste varchar(50),
+	proprietaire bigint
+);
+
+create table Utilisateur(
+	utilisateur_id bigint,
+	pseudo varchar(50),
+	email varchar(50),
+	password varchar(50)
+);
+
+create table Partage(
+	partage_id bigint,
+	musique_id bigint,
+	utilisateur_id bigint
+);
+
+alter table Musique add primary key (musique_id);
+alter table Utilisateur add primary key (utilisateur_id);
+alter table Partage add primary key (partage_id);
+
+alter table Musique add foreign key (proprietaire) references Utilisateur (utilisateur_id);
+alter table Partage add foreign key (musique_id) references Musique (musique_id);
+alter table Partage add foreign key (musique_id) references Utilisateur (utilisateur_id);
