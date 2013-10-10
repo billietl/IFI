164058e15145a055ce01fb8501bb9565be71ca7e
Louis Billiet <louis.billiet59@gmail.com>
2013-10-10 10:58:46 +0200

    suppression des interfaces DAO boiteuses

--- Music/src/fr/univlille1/tiir/music/dao/itf/UtilisateurDAO.java
+++
@@ -1,9 +0,0 @@
-package fr.univlille1.tiir.music.dao.itf;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import fr.univlille1.tiir.music.dao.entities.*;
-
-public interface UtilisateurDAO extends JpaRepository<Utilisateur, Long> {
-
-}
