5661e00b160ea244e24f3f165ffd2aaab728b9b3
florent <florent@pcmt3.m2fil.univ-lille1.fr>
2013-10-14 09:00:46 +0200

    really add user controller

---
+++ Music/src/fr/univlille1/tiir/controller/UserController.java
@@ -0,0 +1,29 @@
+package fr.univlille1.tiir.controller;
+
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import fr.univlille1.tiir.music.dao.JPAConf;
+import fr.univlille1.tiir.music.dao.Repository.UtilisateurRepository;
+import fr.univlille1.tiir.music.dao.entities.Utilisateur;
+
+@Controller
+public class UserController {
+
+
+    @RequestMapping("/user")
+    public @ResponseBody Utilisateur utilisateur(
+            @RequestParam(value="id", required=true, defaultValue="0") Long id) {
+    	AbstractApplicationContext context = new AnnotationConfigApplicationContext(JPAConf.class);
+		UtilisateurRepository repository = context.getBean(UtilisateurRepository.class);
+		Utilisateur user = repository.findOne(id);
+		context.close();
+		
+		return user;
+		}
+}
