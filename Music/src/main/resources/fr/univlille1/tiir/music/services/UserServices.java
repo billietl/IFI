afbfde63da88ae20f0c3ff368f3a4b859ff0ddfb
Florent <florent.david.lille1@gmail.com>
2013-10-31 16:21:05 +0100

    add UserClassService. pas pu tester, pour cause que pom.xml qui fait chier!!!!

---
+++ Music/src/main/resources/fr/univlille1/tiir/music/services/UserServices.java
@@ -0,0 +1,18 @@
+package fr.univlille1.tiir.music.services;
+
+public class UserServices {
+	
+	
+	public void addUser(String pseudo, String email, String passwd)
+	//methode appeler par le controlleur pour ajouter un nouvel utilisateur en base
+	
+	//test findByPseudo
+	// if used -> retourne PseudoUsedError
+	
+	//test findByEmail
+	// if used -> retourne EmailUsedError
+	
+	//else
+	new Utilisateur(String pseudo, String email, String passwd)
+	
+}
