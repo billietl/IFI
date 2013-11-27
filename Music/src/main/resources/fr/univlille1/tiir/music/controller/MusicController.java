b38c4b9e67531e368b6325f8474f6a8e00a40b30
Louis Billiet <louis.billiet59@gmail.com>
2013-11-27 18:36:16 +0100

    enregistrement de modifications???

--- Music/src/main/resources/fr/univlille1/tiir/music/controller/MusicController.java
+++ Music/src/main/resources/fr/univlille1/tiir/music/controller/MusicController.java
@@ -1,26 +1,35 @@
 package fr.univlille1.tiir.music.controller;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import fr.univlille1.tiir.music.dao.JPAConf;
 import fr.univlille1.tiir.music.dao.entities.Musique;
+import fr.univlille1.tiir.music.dao.repository.MusiqueRepository;
 
 @Controller
 public class MusicController {
 	
 	@RequestMapping("/action/listMusic")
 	public String listMusic(Model model){
-		ArrayList<Musique> l = new ArrayList<Musique>(10);
-		Musique m = new Musique();
-		m.setArtiste(artiste);
-		m.setMusiqueId(musiqueId);
-		m.setNotes(notes);
-		m.setTitre(titre);
-		l.add(m);
-		model.addAttribute("musicList", null);
+		AbstractApplicationContext context = new AnnotationConfigApplicationContext(JPAConf.class);
+		MusiqueRepository repository = context.getBean(MusiqueRepository.class);
+		
+		List<fr.univlille1.tiir.music.dto.Musique> l = new ArrayList<fr.univlille1.tiir.music.dto.Musique>(); 
+		
+		for(Musique m : repository.findAll()){
+			l.add(new fr.univlille1.tiir.music.dto.Musique(m));
+		}
+		
+		model.addAttribute("musicList", l);
+		context.close();
+		
 		return "";
 	}
 }
