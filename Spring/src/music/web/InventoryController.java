b735ce6b64e8bf63b2d5fc8686da8dff1a24c824
Louis Billiet <louis.billiet59@gmail.com>
2013-11-30 18:38:35 +0100

    avancement dans le tuto

--- Spring/src/music/web/InventoryController.java
+++ Spring/src/music/web/InventoryController.java
@@ -10,12 +10,18 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import music.service.ProductManager;
+
 import java.io.IOException;
 import java.util.Date;
+import java.util.Map;
+import java.util.HashMap;
 
-public class HelloController implements Controller {
+public class InventoryController implements Controller {
 
     protected final Log logger = LogFactory.getLog(getClass());
+
+    private ProductManager productManager;
 
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -25,7 +31,15 @@ public class HelloController implements Controller {
         String now = (new Date()).toString();
         logger.info("Returning hello view with " + now);
 
-        return new ModelAndView("WEB-INF/jsp/hello.jsp", "now", now);
+        Map<String, Object> myModel = new HashMap<String, Object>();
+        myModel.put("now", now);
+        myModel.put("products", this.productManager.getProducts());
+
+        return new ModelAndView("hello", "model", myModel);
+    }
+
+    public void setProductManager(ProductManager productManager) {
+        this.productManager = productManager;
     }
 
 }
