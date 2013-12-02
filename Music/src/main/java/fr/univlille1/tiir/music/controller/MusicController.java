632a47ecdf8524849ecbf3a6532a41c04e510a03
Louis Billiet <louis.billiet59@gmail.com>
2013-12-02 08:09:20 +0100

    MERGE

--- Music/src/main/java/fr/univlille1/tiir/music/controller/MusicController.java
+++ Music/src/main/java/fr/univlille1/tiir/music/controller/MusicController.java
@@ -1,26 +1,15 @@
 package fr.univlille1.tiir.music.controller;
 
-import java.util.ArrayList;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import fr.univlille1.tiir.music.dao.entities.Musique;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MusicController {
 	
-	@RequestMapping("/action/listMusic")
-	public String listMusic(Model model){
-		ArrayList<Musique> l = new ArrayList<Musique>(10);
-		Musique m = new Musique();
-		m.setArtiste(artiste);
-		m.setMusiqueId(musiqueId);
-		m.setNotes(notes);
-		m.setTitre(titre);
-		l.add(m);
-		model.addAttribute("musicList", null);
-		return "";
+	@RequestMapping("/listMusic")
+	public ModelAndView listMusic(){
+		String m = "Test !!!";
+		return new ModelAndView("musicList", "message", m);
 	}
 }
