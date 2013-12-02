7495c14e8036045703f01f190bc955758a19641f
Louis Billiet <louis.billiet59@gmail.com>
2013-12-02 12:36:38 +0100

    je galere sur le moyen de recup la musique...

---
+++ Music_real/src/fr/univlille1/tiir/music/web/music/MusicRetriever.java
@@ -0,0 +1,27 @@
+package fr.univlille1.tiir.music.web.music;
+
+import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.io.IOException;
+
+public class MusicRetriever implements Controller {
+
+    protected final Log logger = LogFactory.getLog(getClass());
+
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        logger.info("Returning list_music view");
+
+        return null;
+    }
+
+}
