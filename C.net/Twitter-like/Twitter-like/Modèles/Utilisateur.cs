40b6f174db44a01d785fa1650e2538a862d7df6c
billietl <louis.billiet59@gmail.com>
2014-01-13 10:12:05 +0100

    Création du modèle Utilisateur

---
+++ C.net/Twitter-like/Twitter-like/Modèles/Utilisateur.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Twitter_like.Data;
+
+namespace Twitter_like.Modèles
+{
+    public class Utilisateur
+    {
+    }
+}
