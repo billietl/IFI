e34e46b9731997cb95d1ac1ed9b3f41fb39cf575
billietl <louis.billiet59@gmail.com>
2014-01-13 10:31:26 +0100

    Creation d'une methode de hachage

--- C.net/Twitter-like/Twitter-like/Modeles/Utilisateur.cs
+++ C.net/Twitter-like/Twitter-like/Modeles/Utilisateur.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using Twitter_like.Data;
 
@@ -8,6 +10,21 @@ namespace Twitter_like.Modeles
 {
     public class Utilisateur
     {
+        private string CreateMD5Hash(string input)
+        {
+            // Use input string to calculate MD5 hash
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+            // Convert the byte array to hexadecimal string
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                sb.Append(hashBytes[i].ToString("X2")); 
+            }
+            return sb.ToString();
+        }
         public static bool exists(Utilisateur u)
         {
             return false;
@@ -16,6 +33,11 @@ namespace Twitter_like.Modeles
         public static bool is_login_ok(String pseudo, String password)
         {
             return false;
+        }
+
+        public Utilisateur(String pseudo, String password)
+        {
+
         }
     }
 }
