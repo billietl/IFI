5f32b0da18b0b12033035794757fad5b614abe76
billietl <louis.billiet59@gmail.com>
2014-01-20 09:40:42 +0100

    ajout de liens pour se connecter et s'inscrire

---
+++ C.net/Twitter-like/Twitter-like/Controlleurs/loginController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Twitter_like.Controlleurs
+{
+    public class loginController : Controller
+    {
+        //
+        // GET: /login/
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+	}
+}
